721968ba83918750f17cc3b0513d284f1e989b0b
Feng Xiao <xiaofeng9551@outlook.com>
2023-07-08 02:08:52 -0700

    Remove joy, use pwm8

--- src/ROS-PiSBUS_node.cpp
+++ src/ROS-PiSBUS_node.cpp
@@ -1,38 +1,39 @@
 #include "ros/ros.h"
-#include "sensor_msgs/Joy.h"
 #include "ROS-PiSBUS/SBUS.h"
+#include "serial_cppm_array/PWM8.h"
 
-double RC_in[7] = {};
-float channels[16];
+uint16_t RC_in[8] = {};
+uint16_t channels[16];
 
-void joy_callback(const sensor_msgs::Joy &joy_in)
+void ppm_callback(const serial_cppm_array::PWM8 &msg)
 {
-    RC_in[0] = -joy_in.axes[0];
-    RC_in[1] = -joy_in.axes[1];
-    RC_in[2] = -joy_in.axes[2];
-    RC_in[3] = -joy_in.axes[3];
-    RC_in[4] = -joy_in.axes[4];
-    RC_in[5] = -joy_in.axes[5];
-    RC_in[6] = -joy_in.axes[6];
+    RC_in[0] = msg.pwm[0];
+    RC_in[1] = msg.pwm[1];
+    RC_in[2] = msg.pwm[2];
+    RC_in[3] = msg.pwm[3];
+    RC_in[4] = msg.pwm[4];
+    RC_in[5] = msg.pwm[5];
+    RC_in[6] = msg.pwm[6];
+    RC_in[7] = msg.pwm[7];
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "talker");
+    ros::init(argc, argv, "ppm_sbus_covnerter");
     ros::NodeHandle n;
     ros::Rate loop_rate(100);
 
     SBUS::SBUS virtual_t("/dev/ttyAMA0");
     virtual_t.begin();
 
-    ros::Subscriber joy_sub = n.subscribe("joy", 1, joy_callback); // USB RC Remote
+    ros::Subscriber pwm_sub = n.subscribe("ppm_output", 1, ppm_callback); // USB RC Remote
 
     while (ros::ok())
     {
         for (int i = 0; i < 8; i++){
             channels[i] = RC_in[i];
         }
-        virtual_t.writeCal(channels);
+        virtual_t.write(channels);
         ros::spinOnce();
         loop_rate.sleep();
     }
